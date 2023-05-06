0bfd8f4c2b4cf10fdd8d2844411e4c67247eaeeb
chitrang-dev <131484469+chitrang-dev@users.noreply.github.com>
2023-05-05 17:33:14 -0700

    Add files via upload

--- hospital-performance.py
+++ hospital-performance.py
@@ -6,7 +6,7 @@ from snowflake.snowpark.functions import avg, sum, col,lit
 st.set_page_config(
      page_title="Hospital Performance Measures",
      page_icon="🩺",
-     layout="wide",
+     layout="centered",
      initial_sidebar_state="expanded",
      menu_items={
          'Get Help': 'https://developers.snowflake.com',
@@ -20,28 +20,117 @@ def create_session():
     return Session.builder.configs(st.secrets.snowflake).create()
 
 session = create_session()
-st.success("Connected to Snowflake!")
+#st.success("Connected to Snowflake!")
+
+st.header("Hospital Performance Measures")
+
+tab1, tab2, tab3, tab4 = st.tabs(["🌎 Overview", "🏆 Top N","📈 Procedure Trend", "🏥 Hospital Scorecard"])
 
 # Load data table
 @st.cache_data
-def load_data():
-    ## Read in data table
-    table = session.table("HOSPITAL_DB.PUBLIC.STATE_CASE_TREND")
-    
-    ## Collect the results. This will run the query and download the data
-    table = table.collect()
-    return table
+def load_data(tbl):
+    #snow_df1_map = session.table().group_by("hospital","county","longitude","latitude",).sum("num_cases")
+    df = session.table(tbl)
+    return df.to_pandas()
 
-st.header("Hospital Performance Measures")
-## Display data table
 
-df = load_data()
+    ## Display data table
 
-st.bar_chart(df, x="RPT_YEAR")
 
-with st.expander("See Table"):
-    st.dataframe(df)
+    # highest and lowest IMI rating by procedure
+    # lookup ratings by hospital for all procedures with a indicator
+    # plotly map with bubble for cases, procedure selector, rating
+    # https://plotly.com/python/bubble-maps/
+    # https://12ft.io/proxy?&q=https%3A%2F%2Ftowardsdatascience.com%2F3-easy-ways-to-include-interactive-maps-in-a-streamlit-app-b49f6a22a636
+    # county
 
+
+
+with tab1:
+    pd_df1_map = load_data("HOSPITAL_DB.PUBLIC.VW_HOSP_MAP")
+    st.map(pd_df1_map)
+    with st.expander("See Table"):
+        st.dataframe(pd_df1_map)
+
+with tab2:
+    pd_df2_top = load_data("HOSPITAL_DB.PUBLIC.VW_HOSP_RATING")
+    proc_list = pd_df2_top.sort_values(by='PROCEDURE').PROCEDURE.unique()
+    proc_to_filter = st.selectbox('Select a Procedure:', proc_list)
+
+    top_hosp = pd_df2_top[pd_df2_top.PROCEDURE.eq(proc_to_filter) & pd_df2_top.HOSPITAL_RATING.eq('Better')]
+    st.dataframe(top_hosp[["HOSPITAL","COUNTY","RISK_ADJ_MORTALITY"]],use_container_width=True)
+
+    bottom_hosp = pd_df2_top[pd_df2_top.PROCEDURE.eq(proc_to_filter) & pd_df2_top.HOSPITAL_RATING.eq('Worse')].sort_values(by="RISK_ADJ_MORTALITY", ascending=False)
+    st.dataframe(bottom_hosp[["HOSPITAL","COUNTY","RISK_ADJ_MORTALITY"]],use_container_width=True)
+
+with tab3:
+    pd_df3 = load_data("HOSPITAL_DB.PUBLIC.VW_PROC_TREND")
+
+    st.subheader("Acute Stroke")
+    pd_df3_stroke = pd_df3[pd_df3.PROCEDURE.eq("Acute Stroke")]
+    st.line_chart(data=pd_df3_stroke, x="RPT_YEAR", y="AVG_IMI")
+    with st.expander("See Table"):
+        st.dataframe(pd_df3_stroke)
+
+    st.subheader("Acute Myocardial Infarction (AMI)")
+    pd_df3_ami= pd_df3[pd_df3.PROCEDURE.eq("AMI")]
+    st.line_chart(data=pd_df3_ami, x="RPT_YEAR", y="AVG_IMI")
+    with st.expander("See Table"):
+        st.dataframe(pd_df3_ami)
+
+    st.subheader("Heart Failure")
+    pd_df3_hf= pd_df3[pd_df3.PROCEDURE.eq("Heart Failure")]
+    st.line_chart(data=pd_df3_hf, x="RPT_YEAR", y="AVG_IMI")
+
+    st.subheader("Hip Fracture")
+    pd_df3_hip= pd_df3[pd_df3.PROCEDURE.eq("Hip Fracture")]
+    st.line_chart(data=pd_df3_hip, x="RPT_YEAR", y="AVG_IMI")
+
+    st.subheader("PCI")
+    pd_df3_pci= pd_df3[pd_df3.PROCEDURE.eq("PCI")]
+    st.line_chart(data=pd_df3_pci, x="RPT_YEAR", y="AVG_IMI")
+
+    st.subheader("Pneumonia")
+    pd_df3_pn= pd_df3[pd_df3.PROCEDURE.eq("Pneumonia")]
+    st.line_chart(data=pd_df3_pn, x="RPT_YEAR", y="AVG_IMI")
+
+
+# 🔺
+
+with tab4:
+    pd_df4 = load_data("HOSPITAL_DB.PUBLIC.VW_HOSP_DTL")
+    hosp_list = pd_df4.sort_values(by="HOSPITAL").HOSPITAL.unique()
+    hosp_to_filter = st.selectbox('Select a Hospital:', hosp_list)
+    st.write("County: " + pd_df4.iloc[1][1])
+
+    st.subheader("Acute Stroke")
+    pd_df4_hs= pd_df4[pd_df4.HOSPITAL.eq(hosp_to_filter) & pd_df4.PROCEDURE.eq("Acute Stroke")]
+    df_hs = pd_df4_hs[["RPT_YEAR","RATING_INDICATOR","PROCEDURE"]]
+    st.write(df_hs.pivot(index='PROCEDURE', columns='RPT_YEAR', values='RATING_INDICATOR'))
+
+    st.subheader("Acute Myocardial Infarction (AMI)")
+    pd_df4_hs= pd_df4[pd_df4.HOSPITAL.eq(hosp_to_filter) & pd_df4.PROCEDURE.eq("AMI")]
+    df_hs = pd_df4_hs[["RPT_YEAR","RATING_INDICATOR","PROCEDURE"]]
+    st.write(df_hs.pivot(index='PROCEDURE', columns='RPT_YEAR', values='RATING_INDICATOR'))
+
+    st.subheader("Heart Failure")
+    pd_df4_hs= pd_df4[pd_df4.HOSPITAL.eq(hosp_to_filter) & pd_df4.PROCEDURE.eq("Heart Failure")]
+    df_hs = pd_df4_hs[["RPT_YEAR","RATING_INDICATOR","PROCEDURE"]]
+    st.write(df_hs.pivot(index='PROCEDURE', columns='RPT_YEAR', values='RATING_INDICATOR'))
+
+    st.subheader("Hip Fracture")
+    pd_df4_hs= pd_df4[pd_df4.HOSPITAL.eq(hosp_to_filter) & pd_df4.PROCEDURE.eq("Hip Fracture")]
+    df_hs = pd_df4_hs[["RPT_YEAR","RATING_INDICATOR","PROCEDURE"]]
+    st.write(df_hs.pivot(index='PROCEDURE', columns='RPT_YEAR', values='RATING_INDICATOR'))
+
+    st.subheader("Percutaneous Coronary Intervention (PCI)")
+    pd_df4_hs= pd_df4[pd_df4.HOSPITAL.eq(hosp_to_filter) & pd_df4.PROCEDURE.eq("PCI")]
+    df_hs = pd_df4_hs[["RPT_YEAR","RATING_INDICATOR","PROCEDURE"]]
+    st.write(df_hs.pivot(index='PROCEDURE', columns='RPT_YEAR', values='RATING_INDICATOR'))
+
+    st.subheader("Pneumonia")
+    pd_df4_hs= pd_df4[pd_df4.HOSPITAL.eq(hosp_to_filter) & pd_df4.PROCEDURE.eq("Pneumonia")]
+    df_hs = pd_df4_hs[["RPT_YEAR","RATING_INDICATOR","PROCEDURE"]]
+    st.write(df_hs.pivot(index='PROCEDURE', columns='RPT_YEAR', values='RATING_INDICATOR'))
 
 st.caption("Powered by Snowpark for Python and Snowflake Data Marketplace | Made with Streamlit")
-
