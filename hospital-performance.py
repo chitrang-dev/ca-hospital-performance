e4e0aa7df712544097d520ad3635cb2b9b09efe0
chitrang-dev <131484469+chitrang-dev@users.noreply.github.com>
2023-05-05 21:00:03 -0700

    Add files via upload

--- hospital-performance.py
+++ hospital-performance.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from snowflake.snowpark import Session
 from snowflake.snowpark.functions import avg, sum, col,lit
+import plotly.express as px
 
 st.set_page_config(
      page_title="Hospital Performance Measures",
@@ -23,6 +24,7 @@ session = create_session()
 #st.success("Connected to Snowflake!")
 
 st.header("Hospital Performance Measures")
+st.write("Using Inpatient Mortality Indicators (IMI) across the most prevelant disease conditions to compare >300 California Hospitals' Performance")
 
 tab1, tab2, tab3, tab4 = st.tabs(["🌎 Overview", "🏆 Top N","📈 Procedure Trend", "🏥 Hospital Scorecard"])
 
@@ -49,17 +51,21 @@ def load_data(tbl):
 with tab1:
     pd_df1_map = load_data("HOSPITAL_DB.PUBLIC.VW_HOSP_MAP")
     st.map(pd_df1_map)
+    fig = px.scatter_geo(pd_df1_map,lat=pd_df1_map.LATITUDE,lon=pd_df1_map.LONGITUDE, hover_name="HOSPITAL", size="NUM_CASES", scope="usa", fitbounds='locations',locationmode='ISO-3')
+    st.plotly_chart(fig, use_container_width=True)
     with st.expander("See Table"):
         st.dataframe(pd_df1_map)
 
 with tab2:
     pd_df2_top = load_data("HOSPITAL_DB.PUBLIC.VW_HOSP_RATING")
     proc_list = pd_df2_top.sort_values(by='PROCEDURE').PROCEDURE.unique()
-    proc_to_filter = st.selectbox('Select a Procedure:', proc_list)
+    proc_to_filter = st.selectbox('Select a Procedure:', proc_list, index=2)
 
+    st.subheader('Best performing hositals with the lowest Risk Adjusted Mortality')
     top_hosp = pd_df2_top[pd_df2_top.PROCEDURE.eq(proc_to_filter) & pd_df2_top.HOSPITAL_RATING.eq('Better')]
     st.dataframe(top_hosp[["HOSPITAL","COUNTY","RISK_ADJ_MORTALITY"]],use_container_width=True)
 
+    st.subheader('Worst performing hositals with the highest Risk Adjusted Mortality')
     bottom_hosp = pd_df2_top[pd_df2_top.PROCEDURE.eq(proc_to_filter) & pd_df2_top.HOSPITAL_RATING.eq('Worse')].sort_values(by="RISK_ADJ_MORTALITY", ascending=False)
     st.dataframe(bottom_hosp[["HOSPITAL","COUNTY","RISK_ADJ_MORTALITY"]],use_container_width=True)
 
@@ -93,9 +99,6 @@ with tab3:
     st.subheader("Pneumonia")
     pd_df3_pn= pd_df3[pd_df3.PROCEDURE.eq("Pneumonia")]
     st.line_chart(data=pd_df3_pn, x="RPT_YEAR", y="AVG_IMI")
-
-
-# 🔺
 
 with tab4:
     pd_df4 = load_data("HOSPITAL_DB.PUBLIC.VW_HOSP_DTL")
